f97efe0c905e9cf627735a8d82c3d1ae5714834a
fauzia789 <fauziafaiza789@gmail.com>
2024-09-17 23:56:38 +0700

    start work with frontend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const user = require("./routes/user");
 const Books =require("./routes/book");
 const Favourite = require("./routes/favourite")
 const Cart = require("./routes/cart")
+const Order = require("./routes/order")
 // Middleware to parse JSON
 app.use(express.json());
 
@@ -15,6 +16,7 @@ app.use("/api/v1", user);
 app.use("/api/v1", Books);
 app.use("/api/v1", Favourite);
 app.use("/api/v1", Cart);
+app.use("/api/v1", Order);
 
 // Creating port
 app.listen(process.env.PORT, () => {
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            App
+        </div>
+    );
+};
+
+export default App;
