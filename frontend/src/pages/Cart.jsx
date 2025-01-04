7417fde70bfdcd2b787d2b92746644549acee2e7
fauzia789 <fauziafaiza789@gmail.com>
2025-01-04 14:48:43 +0700

    fix url

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -40,7 +40,7 @@ const Cart = () => {
 
   const deleteItem = async (bookid) => {
     try {
-      const response = await axios.put(`http://localhost:3000/api/v1/remove-from-cart/${bookid}`, null, { headers });
+      const response = await axios.put(`http://localhost:1000/api/v1/remove-from-cart/${bookid}`, null, { headers });
       alert(response.data.message);
       await fetchCartData();
     } catch (error) {
